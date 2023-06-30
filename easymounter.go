6ab4be7bd9f824e3ae4a3890a8987ebea4c312ec
hhk02 <80901734+hhk02@users.noreply.github.com>
2023-06-30 20:20:40 +0200

    Add files via upload

--- easymounter.go
+++ easymounter.go
@@ -10,20 +10,29 @@ import (
 )
 
 var source string
-var target string = "/mnt/target"
+var target string
 
 func main() {
 	fmt.Println("EASY MOUNTER by hhk02")
 	fmt.Println("Write source image: ")
 	fmt.Scanln(&source)
+	fmt.Println("Write target path: ")
+	fmt.Scanln(&target)
+
+	if target == "" {
+		fmt.Println("Write target path: ")
+		fmt.Scanln(&target)
+	} else {
+		fmt.Println("Target path: " + target)
+	}
 
 	if source == "" {
 		fmt.Println("Invalid value!")
 		fmt.Println("Write source image: ")
 		fmt.Scanln(&source)
 	} else {
-		fmt.Println("Mounting: " + source)
-		gofsutil.Mount(context.Background(), source, target, "auto", "auto")
+		fmt.Println("Mounting: " + source + "to " + target)
+		gofsutil.Mount(context.Background(), source, target, "auto", "rw,relatime,errors=remount-ro")
 		fmt.Println(("Done!"))
 	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.20
 require github.com/dell/gofsutil v1.12.0
 
 require (
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.9.0 // indirect
 )
